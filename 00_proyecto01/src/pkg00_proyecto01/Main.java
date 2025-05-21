e797ff12374736439d210555347a7cab642dc1be
gjparts <gjparts@gmail.com>
2025-05-21 09:56:56 -0600

    commit 2105202525748

---
+++ 00_proyecto01/src/pkg00_proyecto01/Main.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg00_proyecto01;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //instancia un objeto de clase Mascota
+        Mascota ma1 = new Mascota();
+        System.out.println("Valores en los atributos de ma1:");
+        System.out.println("Direccion de memoria: "+ma1);
+        System.out.println("nombre: "+ma1.nombre);
+        System.out.println("especie: "+ma1.especie);
+        System.out.println("edad: "+ma1.edad);
+        System.out.println("genero: "+ma1.genero);
+        
+        Mascota ma2 = new Mascota();
+        //colocar valores a los atributos de ma2:
+        ma2.nombre = "Chispita";
+        ma2.especie = "Gato";
+        ma2.edad = 2;
+        ma2.genero = 'H';
+        System.out.println("Valores en los atributos de ma2:");
+        System.out.println("Direccion de memoria: "+ma2);
+        System.out.println("nombre: "+ma2.nombre);
+        System.out.println("especie: "+ma2.especie);
+        System.out.println("edad: "+ma2.edad);
+        System.out.println("genero: "+ma2.genero);
+    }
+    
+}
---
+++ 00_proyecto01/src/pkg00_proyecto01/Mascota.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg00_proyecto01;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class Mascota {
+    public String nombre;
+    public String especie;
+    public int edad;
+    public char genero;
+}
