dbd16d0ef460506873a5cbaebf6f02902863d746
GJParts <39961515+gjparts@users.noreply.github.com>
2025-07-08 09:37:32 -0600

    commit 0807202531816

--- 10_herencia2/src/pkg10_herencia2/Video.java
+++ 10_herencia2/src/pkg10_herencia2/Video.java
@@ -1,4 +1,18 @@
 package pkg10_herencia2;
+import java.util.Calendar;
+import java.util.LinkedList;
 public class Video extends Media {
-    
+    //attributes/properties
+    public String codec;
+    public int chapters;
+    public String[] audio;
+    public LinkedList<Subtitle> Subtitle;
+    //constructor
+    public Video(String codec, int chapters, String[] audio, LinkedList<Subtitle> Subtitle, Author author, int duration, String name, long size, String extension, Calendar creationDate) {
+        super(author, duration, name, size, "Video file", extension, creationDate);
+        this.codec = codec;
+        this.chapters = chapters;
+        this.audio = audio;
+        this.Subtitle = Subtitle;
+    }
 }
